d1257fb5076efe991efbb81b51e21ae8c1e3ea29
kyuch <alexkyuchukov@gmail.com>
2023-11-12 17:43:39 -0500

    created variables for the two tabs so we can switch around

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-#opens window, navigated to NetID Admin
+# opens window, navigated to NetID Admin
 driver.get("https://netid.rutgers.edu/adminAuthenticate.htm")
+# mgmt is the name of the NetID management tab
+mgmt = driver.current_window_handle
 # switches to new tab, navigates to OR
 driver.switch_to.new_window('tab')
 driver.get("https://registry.rutgers.edu/viewCompletePerson.htm")
+# reg is the name of the OR tab
+reg = driver.current_window_handle
+driver.switch_to.window(mgmt)
