32e3b4fc7136e58766315708461385e8813d854c
kyuch <alexkyuchukov@gmail.com>
2023-11-13 00:26:34 -0500

    script now enters a netID into the box and loads results

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 options = Options()
 # leaves browser open after task completed
@@ -19,3 +21,24 @@ driver.get("https://registry.rutgers.edu/viewCompletePerson.htm")
 # reg is the name of the OR tab
 reg = driver.current_window_handle
 driver.switch_to.window(mgmt)
+
+
+# put the part below in a function or something so user can call multiple times
+def get_netid_mgmt_info(id):
+    # waits for netID input box to be visible
+    while(len(driver.find_elements("id", 'netID')) < 1):
+        continue
+
+    # HTML input id is netID for box
+    netID_box = driver.find_element("id", 'netID')
+    # input netid argument, press enter. results should load
+    netID_box.send_keys(id + Keys.ENTER)
+
+
+if __name__ == '__main__':
+
+    get_netid_mgmt_info("agk83")
+
+
+
+
